5dbcbb92397ced45cb8b765fa41a45d3a04a236c
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2019-03-07 14:56:42 +0200

    CourseOffering Model migration and validations.

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,42 +12,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_121150) do
-
-  create_table "buildings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_190_307_124_714) do
+  create_table 'buildings', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'course_offerings', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.integer 'term'
+    t.integer 'year'
+    t.bigint 'course_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['course_id'], name: 'index_course_offerings_on_course_id'
   end
 
-  create_table "klass_requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "required_capacity"
-    t.boolean "projector"
-    t.integer "state", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "klass_id"
-    t.index ["klass_id"], name: "index_klass_requests_on_klass_id"
+  create_table 'courses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'code'
+    t.string 'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "klasses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "floor"
-    t.integer "capacity"
-    t.boolean "projector"
-    t.bigint "building_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["building_id"], name: "index_klasses_on_building_id"
+  create_table 'klass_requests', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.integer 'required_capacity'
+    t.boolean 'projector'
+    t.integer 'state', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'klass_id'
+    t.index ['klass_id'], name: 'index_klass_requests_on_klass_id'
   end
 
-  add_foreign_key "klass_requests", "klasses"
-  add_foreign_key "klasses", "buildings"
+  create_table 'klasses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'name'
+    t.string 'floor'
+    t.integer 'capacity'
+    t.boolean 'projector'
+    t.bigint 'building_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['building_id'], name: 'index_klasses_on_building_id'
+  end
+
+  add_foreign_key 'course_offerings', 'courses'
+  add_foreign_key 'klass_requests', 'klasses'
+  add_foreign_key 'klasses', 'buildings'
 end
