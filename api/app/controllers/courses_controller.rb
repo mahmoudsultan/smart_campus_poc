ece790a0927b6fc5b88259f38327ace358a2f554
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2019-04-10 19:36:29 +0200

    modified the controller function to display more needed data.

--- api/app/controllers/courses_controller.rb
+++ api/app/controllers/courses_controller.rb
@@ -6,13 +6,17 @@ class CoursesController < ApplicationController
         Building.connection
         @q = CourseOffering
 
-        @q = @q.joins("INNER JOIN `courses` ON `courses`.`id` = `course_offerings`.`course_id` INNER JOIN `groups` ON "\
-          "`groups`.`course_offering_id` = `course_offerings`.`id` "\
-          "INNER JOIN `group_users` ON `group_users`.`group_id` = `groups`.`id` INNER JOIN `users` ON `users`.`id` = `group_users`.`user_id`"\
+        @q = @q.joins("INNER JOIN `courses` ON `courses`.`id` = `course_offerings`.`course_id` "\
+          "INNER JOIN `groups` ON `groups`.`course_offering_id` = `course_offerings`.`id` "\
+          "INNER JOIN `group_users` ON `group_users`.`group_id` = `groups`.`id`"\
+          "INNER JOIN `users` ON `users`.`id` = `group_users`.`user_id`"\
           "INNER JOIN `lectures` ON `lectures`.`course_offering_id` = `course_offerings`.`id`"\
-          "INNER JOIN `klasses` ON `klasses`.`id` = `lectures`.`klass_id`").where(
-            "`users`.`id`=? AND `users`.`role`='professor' AND `course_offerings`.`term`=? AND `course_offerings`.`year`=?", params[:pid], params[:term], params[:year]
-          ).select('users.name,courses.code,courses.title,klasses.name,klasses.floor,klasses.capacity')
+          "INNER JOIN `klasses` ON `klasses`.`id` = `lectures`.`klass_id`"\
+          "INNER JOIN `buildings` ON `buildings`.`id` = `klasses`.`building_id`").where(
+            "`users`.`id`=? AND `users`.`role`='professor' AND `course_offerings`.`term`=? "\
+            " AND `course_offerings`.`year`=?", params[:pid], params[:term], params[:year]
+          ).select('users.name as user_name,courses.code,courses.title,lectures.day,lectures.start_timeslot,lectures.end_timeslot,'\
+            'buildings.name as building_name,klasses.name as klass_name,klasses.floor,klasses.capacity')
 
         render json: @q
 
