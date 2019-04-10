6518c97a3408ddd3664d6309d4fc933191694eec
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2019-04-10 13:33:42 +0200

    modified for testing.

--- api/app/controllers/courses_controller.rb
+++ api/app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   def show_lectures
     respond_to do |format|
       format.json do 
-        courses = Course.joins(:courseoffering).where(courseoffering: {term: params[:term], year: params[:year]})
+        courses = CourseOffering.joins(:course).where(:term=>params[:term], :year=>params[:year])
         groups = courses.joins(:groups)
         groups_of_prof = groups.joins(:users).where(users: {user_id: params[:pid]})
         lectrures_info = groups_of_prof.joins(lectures: :Klass)
