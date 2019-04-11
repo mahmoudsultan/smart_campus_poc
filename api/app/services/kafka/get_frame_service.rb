1c5f0a7c1913b5324f290ecade681acbf22c152b
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2019-04-11 16:39:42 +0200

    Rubocop is pleased.

--- api/app/services/kafka/get_frame_service.rb
+++ api/app/services/kafka/get_frame_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Kafka
   class GetFrameService
     def initialize(topic_name)
@@ -7,7 +9,7 @@ module Kafka
     def execute
       consumer = Kafka.server.consumer(group_id: 'demo')
       consumer.subscribe(@topic_name, start_from_beginning: false)
-      
+
       consumer.each_message do |message|
         return message.value
       end
--- api/config/initializers/kafka_server.rb
+++ api/config/initializers/kafka_server.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'kafka'
 
 module Kafka
-  SERVERS_ENDPOINTS = ["localhost:9092"]
+  SERVERS_ENDPOINTS = ['localhost:9092'].freeze
   SERVER = Kafka.new(SERVERS_ENDPOINTS)
 
   def self.server
