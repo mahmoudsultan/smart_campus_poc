d9e81dbd02c240f7225f04761faaf8fd1ddd92d8
Karim Elawaad <karim.elawaad@hotmail.com>
2019-04-16 15:57:15 +0200

    added get_attendance function in attendances controller

--- api/app/controllers/attendances_controller.rb
+++ api/app/controllers/attendances_controller.rb
@@ -5,4 +5,47 @@ class AttendancesController < ApplicationController
         render json: results.body
     end
 
+
+    # Takes lecture_instance_id and an optional class_id
+    # Gets a frame from the corresponding class
+    # Gets list of students registered in the course
+    # Sends the frame along with the students' ids to the attendance service
+    # Retuns a JSON containing found students with the corresponding bounding box
+    # coordinates
+    def get_attendance
+        if params[:class_id].nil?
+            @topic_name = get_topic_name_from_class(params[:class_id])
+        else
+            @ret_val = LectureInstance.pluck('klass_id, group_id, course_offering_id')
+                            .joins(:lecture)
+                            .find_by('id' => params[:lecture_instance_id]))
+            
+            @klass_id = @ret_val[0]
+            @group_id = @ret_val[1]
+            @course_offering_id = @ret_val[2]
+
+            @topic_name = get_topic_name_from_class(@klass_id)
+        end
+
+        @frame = Kafka::GetAttendanceFrameService.new(@topic_name).execute
+
+
+
+    end
+
+
+    # Takes a set of IDs along with facebox coordinates
+    # and creates the associated active records
+    def save
+
+    end
+
+    private
+
+    def get_topic_name_from_class(class_id)
+        'random_topic_name'
+    end
+
+
+
 end
