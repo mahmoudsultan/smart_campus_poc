249812de59ae45eec303bd4d211a4d5876d57bf2
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2019-04-17 11:25:59 +0200

    modified controller spec.

--- api/spec/controllers/courses_controller_spec.rb
+++ api/spec/controllers/courses_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
-
-  describe "GET #show_lectures" do
-    it "returns http success" do
-      get :show_lectures
-      expect(response).to have_http_status(:success)
+    describe "GET #show_lectures" do
+      context "given a valid params" do
+        it "returns http success" do
+        get :show_lectures, params: {pid: 135,term: ['fall'], year: ['2018']}
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
