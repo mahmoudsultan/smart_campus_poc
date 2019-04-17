65b2cedaf3180d62c480ae7fa17a96c84b27efc0
Karim Elawaad <karim.elawaad@hotmail.com>
2019-04-17 12:52:01 +0200

    uncommented previously ran seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,97 +1,97 @@
-# # seed courses
-# courses = ActiveSupport::JSON.decode(File.read('db/seeds/courses.json'))
+# seed courses
+courses = ActiveSupport::JSON.decode(File.read('db/seeds/courses.json'))
 
-# courses.each do |a|
-#   Course.create!(a)
-# end
-
-
-# # seed course offerings
-# years = (2017..2019).to_a
-# terms = %i[fall spring summer]
-
-# years.each do |year| 
-#   terms.each do |term|
-#     Course.all.each do |course|
-#       offering = {:course=>course, :term=>term, :year=>year}
-#       CourseOffering.create!(offering)
-#     end
-#   end
-# end
+courses.each do |a|
+  Course.create!(a)
+end
 
 
-# # seed groups
-# CourseOffering.includes(:course).all.each do |offering|
-#   Group.create!(:course_offering=>offering, :name=>"group" + offering.course.title )
-# end
+# seed course offerings
+years = (2017..2019).to_a
+terms = %i[fall spring summer]
+
+years.each do |year| 
+  terms.each do |term|
+    Course.all.each do |course|
+      offering = {:course=>course, :term=>term, :year=>year}
+      CourseOffering.create!(offering)
+    end
+  end
+end
 
 
-# # seed buildings
-# buildings = ["Preparatory Building", "Electricity Building", "Mechanical Building", "SSP Building", "Administrative Building"]
-
-# buildings.each do |building|
-#   Building.create!(:name=>building)
-# end
+# seed groups
+CourseOffering.includes(:course).all.each do |offering|
+  Group.create!(:course_offering=>offering, :name=>"group" + offering.course.title )
+end
 
 
-# # seed klasses
-# klasses = ActiveSupport::JSON.decode(File.read('db/seeds/klass.json'))
-# caps = [60,30]
-# klasses.each do |klass|
-#   klass[:capacity] = caps[rand(2)]
-#   building_offset = rand(Building.count)
-#   rand_build = Building.offset(building_offset).first
-#   klass[:building] = rand_build
-#   Klass.create!(klass)
-# end
+# seed buildings
+buildings = ["Preparatory Building", "Electricity Building", "Mechanical Building", "SSP Building", "Administrative Building"]
 
-# # seed users
-# users = ActiveSupport::JSON.decode(File.read('db/seeds/user.json'))
-
-# users.each_with_index do |a,i|
-#   a[:email] = "aaaaa#{i}@gmail.com"
-#   a[:password] = '123456789'
-#   a[:role] = if rand(2)==1 then "professor" else "student" end
-#   User.create!(a)
-# end
+buildings.each do |building|
+  Building.create!(:name=>building)
+end
 
 
-# # seed lectures
-# days = %i[saturday sunday monday tuesday wednesday thrusday friday]
-# slots = (1..9)
-# slot_size = (0..4)
+# seed klasses
+klasses = ActiveSupport::JSON.decode(File.read('db/seeds/klass.json'))
+caps = [60,30]
+klasses.each do |klass|
+  klass[:capacity] = caps[rand(2)]
+  building_offset = rand(Building.count)
+  rand_build = Building.offset(building_offset).first
+  klass[:building] = rand_build
+  Klass.create!(klass)
+end
 
-# days.each do |day|
-#   i = 0
-#   slots.step(3).each do |slot|
-#     Klass.all.each do |klass|
-#       rand_group = Group.offset(i).first
-#       lecture = {:course_offering=>rand_group.course_offering, :klass=>klass, :group=>rand_group, :day=>day, :start_timeslot=>slot, :end_timeslot=>slot+3}
-#       i+=1
-#       Lecture.create!(lecture)
-#     end
-#   end
-# end
+# seed users
+users = ActiveSupport::JSON.decode(File.read('db/seeds/user.json'))
 
-# #seed lecture_instances
+users.each_with_index do |a,i|
+  a[:email] = "aaaaa#{i}@gmail.com"
+  a[:password] = '123456789'
+  a[:role] = if rand(2)==1 then "professor" else "student" end
+  User.create!(a)
+end
 
-# def rand_time(from, to=Time.now)
-#     Time.at(rand_in_range(from.to_f, to.to_f))
-# end
 
-# def rand_in_range(from, to)
-#     rand * (to - from) + from
-#   end
+# seed lectures
+days = %i[saturday sunday monday tuesday wednesday thrusday friday]
+slots = (1..9)
+slot_size = (0..4)
 
-# week_numbers = (1..14)
+days.each do |day|
+  i = 0
+  slots.step(3).each do |slot|
+    Klass.all.each do |klass|
+      rand_group = Group.offset(i).first
+      lecture = {:course_offering=>rand_group.course_offering, :klass=>klass, :group=>rand_group, :day=>day, :start_timeslot=>slot, :end_timeslot=>slot+3}
+      i+=1
+      Lecture.create!(lecture)
+    end
+  end
+end
 
-# Lecture.all.each do |lecture|
-#     week_numbers.each do |week|
-#         lecture_date = rand_time((14-week).days.ago)
-#         lecture_instance = {:lecture => lecture, :date => lecture_date, :week_number => week}
-#         LectureInstance.create!(lecture_instance)
-#     end
-# end
+#seed lecture_instances
+
+def rand_time(from, to=Time.now)
+    Time.at(rand_in_range(from.to_f, to.to_f))
+end
+
+def rand_in_range(from, to)
+    rand * (to - from) + from
+  end
+
+week_numbers = (1..14)
+
+Lecture.all.each do |lecture|
+    week_numbers.each do |week|
+        lecture_date = rand_time((14-week).days.ago)
+        lecture_instance = {:lecture => lecture, :date => lecture_date, :week_number => week}
+        LectureInstance.create!(lecture_instance)
+    end
+end
 
 
 # seed group_users
