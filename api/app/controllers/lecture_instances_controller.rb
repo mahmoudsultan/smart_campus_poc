bf38585922607beac3246bfecbb611ce034831ed
Karim Elawaad <karim.elawaad@hotmail.com>
2019-04-19 19:05:43 +0200

    added lecture instances controller

--- api/app/controllers/lecture_instances_controller.rb
+++ api/app/controllers/lecture_instances_controller.rb
@@ -1,13 +1,14 @@
 class LectureInstancesController < ApplicationController
     before_action :set_lecture_instance, only: %i[place]
     def place
-        place = @lecture_instance.klass.pluck(:name)
+        place = @lecture_instance.klass.name
 
         render json: { place: place }, status: :ok
     end
 
+    private
     def set_lecture_instance
-        @lecture_instance = LectureInstance.find params[:lecture_instance_id]
+        @lecture_instance = LectureInstance.find params[:id]
     end
 
 end
