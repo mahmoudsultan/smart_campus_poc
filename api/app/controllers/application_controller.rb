8bf4a69ed3f700ed1771811c8bdf0c236634405c
alyswidan <swidan12340@gmail.com>
2019-04-24 22:04:58 +0200

    devise auth token permits name and department

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,4 +3,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   devise_token_auth_group :member, contains: %i[user admin]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit the `subscribe_newsletter` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :department])
+  end
 end
