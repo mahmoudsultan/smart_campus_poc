03ae772ed3a15382586e8e5b11d092c0a5beaed1
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2019-04-27 23:20:44 +0200

    modified route to handle both students and professors as a user role.

--- api/app/controllers/courses_controller.rb
+++ api/app/controllers/courses_controller.rb
@@ -4,6 +4,8 @@ class CoursesController < ApplicationController
     Building.connection
     @q = CourseOffering
 
+    @role = User.where(id: params[:id]).select(:role)
+
     @q = @q.joins("INNER JOIN `courses` ON `courses`.`id` = `course_offerings`.`course_id` "\
       "INNER JOIN `groups` ON `groups`.`course_offering_id` = `course_offerings`.`id` "\
       "INNER JOIN `group_users` ON `group_users`.`group_id` = `groups`.`id`"\
@@ -11,8 +13,8 @@ class CoursesController < ApplicationController
       "INNER JOIN `lectures` ON `lectures`.`course_offering_id` = `course_offerings`.`id`"\
       "INNER JOIN `klasses` ON `klasses`.`id` = `lectures`.`klass_id`"\
       "INNER JOIN `buildings` ON `buildings`.`id` = `klasses`.`building_id`").where(
-        "`users`.`id`=? AND `users`.`role`=? AND `course_offerings`.`term`=? "\
-        " AND `course_offerings`.`year`=?", params[:email], ,params[:role], params[:term], params[:year]
+        "`users`.`id`=? AND `users`.`role`=(?) AND `course_offerings`.`term`=? "\
+        " AND `course_offerings`.`year`=?", params[:id], @role, params[:term], params[:year]
       ).select('users.id as user_id, users.name as user_name,courses.code,courses.title,lectures.day,'\
                'lectures.start_timeslot,lectures.end_timeslot,'\
                'buildings.name as building_name,klasses.name as klass_name,klasses.floor,klasses.capacity,'\
