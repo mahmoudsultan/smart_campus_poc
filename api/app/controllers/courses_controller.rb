41937098b035cd6bd162968b78abab6da6ae1d7f
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2019-05-20 12:58:26 +0200

    modified coursescontroller to a more ruby code instead of the mysql query.

--- api/app/controllers/courses_controller.rb
+++ api/app/controllers/courses_controller.rb
@@ -4,19 +4,13 @@ class CoursesController < ApplicationController
     Building.connection
     @q = CourseOffering
 
-    @q = @q.joins("INNER JOIN `courses` ON `courses`.`id` = `course_offerings`.`course_id` "\
-      "INNER JOIN `groups` ON `groups`.`course_offering_id` = `course_offerings`.`id` "\
-      "INNER JOIN `group_users` ON `group_users`.`group_id` = `groups`.`id`"\
-      "INNER JOIN `users` ON `users`.`id` = `group_users`.`user_id`"\
-      "INNER JOIN `lectures` ON `lectures`.`course_offering_id` = `course_offerings`.`id`"\
-      "INNER JOIN `klasses` ON `klasses`.`id` = `lectures`.`klass_id`"\
-      "INNER JOIN `buildings` ON `buildings`.`id` = `klasses`.`building_id`").where(
-        "`users`.`id`=? AND `users`.`role`='professor' AND `course_offerings`.`term`=? "\
-        " AND `course_offerings`.`year`=?", params[:pid], params[:term], params[:year]
-      ).select('users.id as user_id, users.name as user_name,courses.code,courses.title,lectures.day,'\
-               'lectures.start_timeslot,lectures.end_timeslot,'\
-               'buildings.name as building_name,klasses.name as klass_name,klasses.floor,klasses.capacity,'\
-               'lectures.id as lec_id')
+    @q = CourseOffering.joins({ lectures: {klass: :building} }, :course, groups: { group_users: :user } ).where(users: { uid: request.headers[:uid] },
+      course_offerings: { term: params[:term], year: params[:year] })
+      .select('users.id as user_id', 'users.name as user_name','courses.code',
+              'courses.title','lectures.day', 'lectures.start_timeslot',
+              'lectures.end_timeslot','buildings.name as building_name',
+              'klasses.name as klass_name','klasses.floor','klasses.capacity',
+              'lectures.id as lec_id')
 
     render json: @q
   end
